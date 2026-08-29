ca5a164386278e164d5ad9db792950997c2abbed
dev-ashir768 <techashir167@gmail.com>
2026-08-29 21:38:44 +0500

    feat: add new regions data component with international flag mappings

--- data/regions.tsx
+++ data/regions.tsx
@@ -1,24 +1,25 @@
 const RegionsData = () => {
   return [
+    // North America
     { name: "United States", icon: "/flags/usa.jpg" },
+    { name: "Mexico", icon: "/flags/mexio.jpg" },
+    
+    // Europe
     { name: "United Kingdom", icon: "/flags/uk.jpg" },
-    // { name: "Belgium", icon: "/flags/belgium.jpg" },
-    // { name: "Brazil", icon: "/flags/brazil.jpg" },
+    { name: "Ireland", icon: "/flags/ireland.jpg" },
     { name: "Germany", icon: "/flags/germany.jpg" },
     { name: "France", icon: "/flags/france.jpg" },
     { name: "Spain", icon: "/flags/spain.jpg" },
     { name: "Italy", icon: "/flags/italy.jpg" },
-    // { name: "Japan", icon: "/flags/japan.jpg" },
-    { name: "Ireland", icon: "/flags/ireland.jpg" },
     { name: "Netherlands", icon: "/flags/netherland.jpg" },
+    
+    // Asia
     { name: "Singapore", icon: "/flags/singapore.jpg" },
     { name: "Malaysia", icon: "/flags/malaysia.jpg" },
     { name: "Philippines", icon: "/flags/philippines.jpg" },
-    // { name: "Poland", icon: "/flags/poland.jpg" },
     { name: "Thailand", icon: "/flags/thailand.jpg" },
     { name: "Vietnam", icon: "/flags/vietnam.jpg" },
     { name: "Indonesia", icon: "/flags/indonesia.jpg" },
-    { name: "Mexico", icon: "/flags/mexio.jpg" },
   ];
 };
 
